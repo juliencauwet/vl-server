b87214d37ca75fada4766c8c48f9a645fa3e5155
julien <juliencauwet@yahoo.fr>
2019-04-10 23:42:37 +0200

    added Jenkinsfile, shell scripts for CI/CD

---
+++ Jenkinsfile
@@ -0,0 +1,21 @@
+pipeline {
+    agent any
+    stages {
+        stage('checkout') {
+            steps {
+                git 'https://github.com/juliencauwet/vl-server.git'
+            }
+        }
+        stage('build'){
+            steps {
+                sh './build.sh'
+            }
+        }
+        stage('deploy'){
+            steps {
+                sh './deploy.sh'
+            }
+        }
+
+    }
+}
---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "************************************************************************"
+echo "**************************** Building phase ****************************"
+echo "************************************************************************"
+
+cd ..
+
+docker  run --rm \
+        -v /var/run/docker.sock:/var/run/docker.sock \
+        -v $PWD/config-server:/config-server \
+        -v ~/.m2/repository:/root/.m2/ \
+        -w /config-server maven:3-alpine "$@"
