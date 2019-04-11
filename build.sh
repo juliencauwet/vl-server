58cf1a01b8c0dff800b5503cd55d8c4bf807a638
julien <juliencauwet@yahoo.fr>
2019-04-11 11:15:33 +0200

    ok

--- build.sh
+++ build.sh
@@ -4,16 +4,18 @@ echo "************************************************************************"
 echo "**************************** Building phase ****************************"
 echo "************************************************************************"
 
-pwd
-ls
-cat pom.xml
+mvn clean install
 
-docker  run --rm \
-        -v /var/run/docker.sock:/var/run/docker.sock \
-        -v $PWD/:/config-server/ \
-        -v ~/.m2/repository:/root/.m2/ \
-        -w /config-server/ \
-        maven:3-alpine "$@"
+#pwd
+#ls
+#cat pom.xml
+#
+#docker  run --rm \
+#        -v /var/run/docker.sock:/var/run/docker.sock \
+#        -v $PWD/:/config-server/ \
+#        -v ~/.m2/repository:/root/.m2/ \
+#        -w /config-server/ \
+#        maven:3-alpine "$@"
 
 
 #docker run --rm -v $PWD/java-app:/app -v /root/.m2/:/root/.m2/ -w /app  maven:3-alpine "$@"
