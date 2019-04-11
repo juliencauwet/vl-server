6106f88bf69e34b2c0df942c8b09dab1f2c2992d
julien <juliencauwet@yahoo.fr>
2019-04-11 14:52:18 +0200

    ok

--- build.sh
+++ build.sh
@@ -11,6 +11,7 @@ ls
 
 docker  run --rm \
         -v /var/run/docker.sock:/var/run/docker.sock \
+        -v $PWD:/vl-server \
         -v ~/.m2/repository:/root/.m2/ \
         -w /vl-server \
         maven:3-alpine "$@"
