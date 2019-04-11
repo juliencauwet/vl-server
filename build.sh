e29e452c097a06780e5a56574340bb06739855cb
julien <juliencauwet@yahoo.fr>
2019-04-11 12:34:54 +0200

    ok

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ pwd
 
 docker  run --rm \
         -v /var/run/docker.sock:/var/run/docker.sock \
-        -v /:/config-server \
+        -v $PWD:/config-server \
         -v ~/.m2/repository:/root/.m2/ \
         -w /config-server \
         maven:3-alpine "$@"
